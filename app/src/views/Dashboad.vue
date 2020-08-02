60ca189069e5dcc07b21a694927340e4e9a6bd85
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2020-08-02 15:05:51 -0300

    [0.0.5] Define dashboard main struture

--- app/src/views/Dashboad.vue
+++ app/src/views/Dashboad.vue
@@ -1,6 +1,37 @@
 <template>
-  <div id="dashboard" class="columns">
-    <span>Loged</span>
+  <div class="tile is-ancestor">
+    <div class="tile is-vertical is-2">
+      <div class="tile">
+        <div class="tile is-parent is-vertical">
+          <article class="tile is-child notification is-primary">
+            <div class="content">
+              <p class="title">Menu</p>
+              <div class="content">
+                <!-- Content -->
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+    <div class="tile is-parent">
+      <article class="tile is-child notification is-warning is-2">
+        <div class="content">
+          <p class="title">Sub Menu</p>
+          <div class="content">
+            <!-- Content -->
+          </div>
+        </div>
+      </article>
+      <article class="tile is-child notification is-success">
+        <div class="content">
+          <p class="title">List</p>
+          <div class="content">
+            <!-- Content -->
+          </div>
+        </div>
+      </article>
+    </div>
   </div>
 </template>
 <script>
@@ -10,9 +41,7 @@ export default {
 };
 </script>
 <style scoped>
-#dashboard {
-  margin: auto;
-  width: 50%;
-  padding: 10px;
+.is-ancestor {
+  margin: 5px;
 }
 </style>
